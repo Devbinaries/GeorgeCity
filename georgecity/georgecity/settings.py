ddf2074f333bd55c0f80cc5b975bf4d272b55f71
gerardzikpi <kodinapetra@yahoo.com>
2026-07-13 11:51:50 -0700

    Add missing comma to ALLOWED_HOSTS in settings.py

--- georgecity/georgecity/settings.py
+++ georgecity/georgecity/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = [
     "0.0.0.0"
     "127.0.0.1",
     "127.0.0.1:8000",
-    "http://georgecity-server.onrender.com"
+    "http://georgecity-server.onrender.com",
+    "georgecity.onrender.com",
 ]
 
 
